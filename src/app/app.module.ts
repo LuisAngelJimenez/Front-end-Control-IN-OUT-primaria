d036a68f932bc512722a27d08ad8a8ddf428431f
LuisAngelJimenez <luisangelastellanos003@gmail.com>
2024-07-15 19:17:24 -0600

    fixing tabs modulated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IonicStorageModule } from '@ionic/storage-angular';
 
-@NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, IonicStorageModule.forRoot()],
 import { FormsModule } from '@angular/forms';
 import { LoginPage } from './login/login.page';
-import { IonicStorageModule } from '@ionic/storage-angular';
-
 
 @NgModule({
   declarations: [AppComponent,LoginPage],
