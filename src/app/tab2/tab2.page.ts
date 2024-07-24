423737b55822389feab19527fd6830fb2565b13e
PATIMOM0 <gaeloso290@gmail.com>
2024-07-23 19:23:56 -0600

    mejorando el historial de la aplicacion

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -3,9 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  styleUrls: ['tab2.page.scss'],
 })
 export class Tab2Page {
+
+  personas = [
+    { nombre: 'Jesus Perez', edad: 10, email: 'jesus@gmail.com',grupo:'B',tutor:'Susana Perez' },
+    { nombre: 'Omar Arellanes', edad: 12, email: 'omar@gmail.com',grupo:'A',tutor:'Alex Arellanes' },
+    { nombre: 'Saul Uziel', edad: 9, email: 'saul@gmail.com',grupo:'C',tutor:'Isidro Uziel' },
+    { nombre: 'Luis Diaz', edad: 7, email: 'luis@gmail.com',grupo:'A',tutor:'Bruno Diaz' }
+  ];
 
   constructor() {}
 
